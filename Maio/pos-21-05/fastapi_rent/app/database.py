3f37773ed6314f8d1a289266fc3758cc69d58087
Estela Áurea <144624518+Sra-Estela@users.noreply.github.com>
2025-06-25 15:44:49 -0300

    Delete Maio directory

--- Maio/pos-21-05/fastapi_rent/app/database.py
+++
@@ -1,6 +0,0 @@
-from typing import List
-from app.models import Carro, Cliente, Reserva
-
-carros: List[Carro] = []
-clientes: List[Cliente] = []
-reservas: List[Reserva] = []
--- Maio/pos-21-05/fastapi_rent/app/main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from app.routers import carros, clientes, reservas
-
-app = FastAPI(title="Sistema de Reserva de Carros")
-
-app.include_router(carros.router)
-app.include_router(clientes.router)
-app.include_router(reservas.router)
